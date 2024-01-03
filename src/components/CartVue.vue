5e2e2be8fb5a3d070bb2fc03bc74272dad01a121
Fodi999 <fodi85999@gmail.com>
2024-01-03 23:09:45 +0100

    header fix

--- src/components/CartVue.vue
+++ src/components/CartVue.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <template>
-   <section class=" lg:flex  lg:space-x-2 my-6  border-b px-2 border-sky-900">
+   <section class=" lg:flex  lg:space-x-2 my-2  px-2 border-sky-900">
            
      <a class="w:full lg:w-1/3 bg-gra h-96 mb-4 relative group rounded inline-block overflow-hidden " href="#">
       <div class="absolute top-0 right-0  font-semibold  text-sky-900 hover:text-white hover:bg-sky-900 pr-2">{{ currentDate }}</div>
@@ -19,9 +19,9 @@ export default {
             
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
                
-              <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">Food Business</h4>
+              <h4 class=" relative mb-20 top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">Food Business</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Dima Fomin</p>
                 <time class=" text-sky-950 normal-case sm:text-xs">
@@ -39,19 +39,19 @@ export default {
         </a>
         <a class="lg:w-1/3 bg-gra h-96 mb-4 relative group rounded inline-block overflow-hidden " href="#">
           <div class="absolute top-0 right-0  font-semibold  text-sky-900 hover:text-white hover:bg-sky-900  pr-2">{{ currentDate }}</div>
-            <img class="w-full h-auto transition-opacity duration-300 group-hover:opacity-40" src="/Rectangle 15.png" alt="" />  
+            <img class="w-full h-auto transition-opacity duration-300 group-hover:opacity-40" src="/Rectangle 33.png" alt="" />  
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
-              <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
+              <h4 class=" relative mb-32 top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">Seviche</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
-                  Chef with extensive experience in the food and culinary industry. My culinary journey spans over 20 years, 
-                  during which I have worked in various countries and immersed myself in numerous technologies for creating new taste experiences.
+                  Ceviche is an exquisite Latin American dish consisting of fresh seafood marinated in a sauce based on citrus juice. 
+                  In this version we use Scallop and Carabinieri to create a sophisticated dish with exquisite taste and aroma.
                 </time>
                 <router-link to="/list">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600 '></i>
                   </button>
                 </router-link>
@@ -66,7 +66,7 @@ export default {
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -75,7 +75,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600'></i> 
                   </button>
                 </router-link>
@@ -89,7 +89,7 @@ export default {
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -98,7 +98,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600'></i> 
                   </button>
                 </router-link>
@@ -108,11 +108,11 @@ export default {
     <section class=" lg:flex lg:space-x-2 my-2 px-2 border-b  border-sky-900">
             <a class="lg:w-1/3 bg-gra h-96 relative group rounded inline-block overflow-hidden " href="#">
               <div class="absolute top-0 right-0  font-semibold  text-sky-900 hover:text-white hover:bg-sky-900  pr-2">{{ currentDate }}</div>
-            <img class="w-full h-auto transition-opacity duration-300 group-hover:opacity-40" src="/Rectangle 33.png" alt="" />  
+            <img class="w-full h-auto transition-opacity duration-300 group-hover:opacity-40" src="/Rectangle 15.png" alt="" />  
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -121,7 +121,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600 '></i> 
                   </button>
                 </router-link>
@@ -135,7 +135,7 @@ export default {
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -144,7 +144,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600 '></i> 
                   </button>
                 </router-link>
@@ -158,7 +158,7 @@ export default {
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -167,7 +167,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600'></i> 
                   </button>
                 </router-link>
@@ -181,7 +181,7 @@ export default {
             <div class="p-4 absolute bottom-0 left-0 z-20 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
               <h4 class=" relative mb-32  top-0 left-0 text-1xl font-semibold text-sky-950 cursor-pointer leading-tight">IT&Cook-Fodifood</h4>
                 <div class="flex mt-3">
-                  <img class="h-10 w-10 rounded-full mr-2 object-cover" src="/Ellipse 1.png" alt="">
+                  <img class="h-10 w-10 rounded-full mr-4 object-cover" src="/Ellipse 1.png" alt="">
                 </div>
                 <p class="font-semibold  text-sky-950 text-sm">Domian Fomin</p>
                 <time class=" text-sky-950 normal-case text-xs">
@@ -190,7 +190,7 @@ export default {
                   и поделится с вами идеями и вкусами.
                 </time>
                 <router-link to="/about">
-                  <button class="flex justify-center ml-64">
+                  <button class="flex justify-center sm:ml-64">
                     <i class='cursor-pointer transition-transform hover:scale-110 bx bx-log-in-circle bx-md  text-red-600 hover:text-green-600'></i> 
                   </button>
                 </router-link>
--- src/views/ListVue.vue
+++ src/views/ListVue.vue
@@ -1,5 +1,5 @@
 <script setup>
-import MainVue from '@/components/MainVue.vue';
+
 
 const downloadImage1 = () => {
   const imageUrl = '/Rectangle 33.png';
@@ -18,78 +18,127 @@ defineExpose({
 
 <template>
   <div class="bg-white max-w-screen-xl mx-auto">
-  <header class="px-0 border-b border-sky-900 flex items-center justify-between h-14">
+  <header class="flex px-0 border-b border-sky-900 items-center justify-between h-14">
         <router-link class=" text-green-600" to="/">
           <button>
             <img class="m-2 w-10 h-10 transition-transform hover:scale-150" src="/Ellipse 50.png" alt="logo" />
           </button>
         </router-link>
-        <h1 class="ml-40  uppercase font-bold text-sky-900 cursor-pointer transition-all hover:scale-150" href="#">IT & Cook</h1>
+        <h1 class="ml-40 uppercase font-bold text-sky-900 cursor-pointer text-3xl transition-all hover:scale-150" href="#">IT & Cook</h1>
         <nav class="hidden md:flex items-center">
           <ul class="text-gray-500 font-semibold inline-flex items-center">
+            <a href="https://www.instagram.com/fodifood" target="_blank" rel="noopener noreferrer">
+          <i class='cursor-pointer m-2 transition-transform hover:scale-150 bx bxl-instagram bx-sm  text-orange-600'></i>
+        </a>
           <a href="https://www.instagram.com/fodifood" target="_blank" rel="noopener noreferrer">
-            <img class="cursor-pointer m-2 w-6 h-6 transition-transform hover:scale-150" src="/Vector 3.svg" alt="">
+            <i class='cursor-pointer m-2 transition-transform hover:scale-150 bx bxl-facebook-circle bx-sm text-blue-800 '></i>
           </a>
           <a href="https://www.instagram.com/fodifood" target="_blank" rel="noopener noreferrer">
-            <img class="cursor-pointer m-2 w-6 h-6 transition-transform hover:scale-150" src="/Vector 2.svg" alt="">
+            <i class='cursor-pointer m-2 transition-transform hover:scale-150 bx bxl-gmail bx-sm  text-purple-800' ></i>
           </a>
-          <a href="https://www.instagram.com/fodifood" target="_blank" rel="noopener noreferrer">
-            <img class="cursor-pointer m-2 w-6 h-6 transition-transform hover:scale-150" src="/Vector 1.svg" alt="">
-          </a>
-          
-
-
-          </ul>
-          <ul class="inline-flex items-center">
-            <li><button class="ml-2 py-1 px-3 border-2  font-bold rounded-full border-sky-900 text-sky-900 hover:text-white hover:bg-sky-900 transition-all duration-1000">Login</button></li>
-            <li><button class="ml-2 py-1 px-3 border-2  font-bold rounded-full border-sky-900 text-sky-900 hover:text-white hover:bg-sky-900 transition-all duration-1000">Register</button></li>
-          </ul>
+          </ul>      
         </nav>
          </header> 
-         <MainVue/>
+         
         <div>
-          <div class="py-4 overflow-y-auto whitespace-nowrap">
+          <div class="py-4 sm:overflow-y-auto whitespace-nowrap">
           
           
           </div>
         </div>
-        <section class=" px-2">
-          <a class="flex mb-10" href="">
-            <img class=" h-96  object-cover mr-4" src="/Rectangle 15.png" alt="">
+        <section class="flex px-2">
+          <a class="lg:flex mb-10" href="">
+            <img class=" h-96  object-cover mr-4" src="/Rectangle 33.png" alt="">
             <div class="max-w-screen-xl mx-auto">
                 <button @click="downloadImage1" class="flex items-center space-x-1 border-1 rounded-full font-bold py-1.5 px-1.5 border-sky-800 text-sky-800 hover:text-white hover:bg-sky-900" onClick={handleDownload}>
               <img class="w-8 h-8 cursor-pointer transition-transform hover:scale-110" src="/Ellipse 50.png" alt="feis" />
-              <span class="px-2 text-xs">Dowenload & New menu</span>
+              <span class="px-2 text-xs">Dowenload & Recipe</span>
               </button>
               <div class="">
                 
-                <h3 class=" mt-2 mb-2 border-b border-sky-900 text-gray-700 font-bold text-2xl"> Фомин Дмитрий Шеф повар </h3>
+                <h3 class=" mt-2 mb-2 border-b border-sky-900 text-gray-700 font-bold text-2xl">Seviche with Wild Scallops and Carabineros</h3>
                 
-                <p class="text-gray-700 text-wrap mb-2">Этот Блог написан для тех ,кто готов открыть для себя восхитительные комбинаций вкусов .
-                   Хочу рассказать и  научить вас готовить новые блюда, которые я придумываю и готовлю для того чтоб удивить вас , и поделится с вами идеями и вкусами.
-                   Для каждого повара, тем паче домашнего, возможность творить из того, что есть под рукой, всегда удивительно и прекрасно.
-                   Строгие рецепты ограничивают творчество. До недавнего времени методом проб и ошибок был единственным способом узнать, какие продукты будут хорошо сочетаться между собой. 
-                   А украсить своим творением семейный стол - двойное удовольствие. Новое всегда будет радостью для своих любимых и друзей. 
-                   И независимо от того, что у вас в результате получится, вы можете быть твердо уверены в одном: это всегда будет вкуснее купленного в супермаркете.
-                   Как и в любом деле, здесь также важно умение обращаться с инвентарем и знание технологического процесса. 
-                   Поварское ремесло не отличается от других: вашими инструментами могут быть голые руки, а принадлежностями продукты, но при этом также важно знать то, как они должны действовать и взаимодействовать с другими компонентами. 
-                   Я расскажу с какими продуктами я работаю, и какую пользу они приносят нам. Чтобы не начинать с чистого листа, Вы сможете брать эти рецепты за основу и создавать новые кулинарные шедевры.
-                   Вы опытный повар или новичек ? Кем бы вы ни были , надеюсь этот Блог поможет вам раскрыть ваши таланты .
-                   Добавьте щепотку науки о вкусе в свое блюдо знаний ! Пусть этот Блог откроет вам мир новых гастрономических изысков.
-               </p>
-              
+                <div class="text-gray-700 text-wrap sm:text-left mb-2">
+                  
+<p class="text-orange-600 font-bold text-lg">
+Preparation Time: 20 minutes
+Servings: 2 
+</p>
+<p class="text-indigo-800 font-bold">Ingredients:</p>
+<ul class="p-2">
+  <li class="list-disc">Carabineros (peeled) - 2 pcs.</li>
+</ul>
 
+<ul class="p-2">
+  <li class="list-disc">Sea Scallops - 6 pcs.</li>
+</ul>
+<p></p>
+<p class="text-indigo-800 font-bold">
+For the Marinade:
+</p>
+<ul class="p-2">
+  <li class="list-disc">Olive oil - 1 tbsp.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Orange - 2 pcs.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Orange pepper - 2 pinches.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Sea salt - 1/2 tsp.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Chili pepper - 1 pc.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Shallot onion (medium-sized) - 1 pc.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Cilantro - 2 sprigs.</li>
+</ul>
+<ul class="p-2">
+  <li class="list-disc">Red caviar for garnish - 1 tbsp.</li>
+</ul>
+<p class="text-indigo-800 font-bold">Step-by-Step Recipe:</p>
+<p class="text-indigo-800">Step 1: Prepare Seafood</p>
+
+<p class=" underline font-bold">
+Pat dry two shrimps with a paper towel and cut them into cubes.
+Cut Sea Scallops into cubes as well and place them in a bowl.
+</p>
+<p class="text-indigo-800">Step 2: Make the Marinade</p>
+<p class="underline font-bold">
+Squeeze juice from oranges and strain it through a sieve into a container.
+Dice shallot finely, remove seeds from chili pepper, and dice it too.
+Finely chop cilantro leaves.
+Add all ingredients to orange juice: shallot, chili, cilantro, salt, orange pepper, and olive oil. Mix well. 
+</p>
+<p class="text-indigo-800">Step 3: Marinate the Seafood</p>
+<p class="underline font-bold">
+Add the marinade to the diced seafood, mix well.
+Put it in the refrigerator for 10 minutes to marinate. 
+</p>
+<p class="text-indigo-800">
+Step 4: Serving 
+</p>
+<p class="underline font-bold">
+ Arrange the marinated seafood on a plate.
+Pour the remaining marinade over it.
+Garnish with chili pepper, cilantro, and add red caviar.
+
+</p>
+<p class=" text-center font-bold text-orange-600 ">
+Serve and enjoy!
+This dish will be a delightful treat for you and your loved ones. 
+Bon appétit! 
+</p>
+            </div>
               </div>
 
             </div>
           </a>
         </section>
-        
-
-
-         
-   
-   
       </div>
       <p class=" mt-14 flex items-center justify-center text-xs  text-sky-900">&copy;2023 FoDifood-Created the design and wrote the website All rights reserved</p>
    </template>
