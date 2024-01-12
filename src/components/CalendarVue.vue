f1953f1c51187fb3c06f1cbe1dc9fac0640f9f35
Fodi999 <fodi85999@gmail.com>
2024-01-12 16:26:25 +0100

    header fix

--- src/components/CalendarVue.vue
+++ src/components/CalendarVue.vue
@@ -66,8 +66,9 @@ const isOpen = (day) => {
         
        <HomeHeader />
         </nav>
-    <div class="flex flex-col items-center justify-center p-4">
-      <div class="bg-white rounded-lg shadow p-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-10">
+  
+    <div class="bg-white rounded-lg shadow p-4 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
         <header class="flex justify-between border-b-2 border-b-sky-900 p-2 items-center">
           <button @click="decreaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
             <i class='bx bx-left-arrow-alt'></i>
@@ -92,11 +93,123 @@ const isOpen = (day) => {
             <template v-for="(value, index) in (firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1)" :key="index">
     <div class="w-8 h-8"></div>
 </template>
-            <button class="border-2  border-black rounded-full w-8 h-8 flex items-center justify-center" v-for="(day,) in daysInMonth" :key="day" 
-                    :class="isOpen(day) ? ' text-yellow-200 bg-green-600 rounded-full w-8 h-8 flex items-center justify-center' : 'bg-red-500 text-black rounded-full w-8 h-8 flex items-center justify-center'">
-                {{ day }}
-            </button>
+<button class="border-2 border-blue-800 rounded-full w-8 h-8 flex items-center justify-center" 
+        v-for="(day,) in daysInMonth" 
+        :key="day" 
+        :class="isOpen(day) ? ' bg-green-600 rounded-full w-8 h-8 flex items-center justify-center' : 'bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center'"
+        @click="isOpen(day) && $router.push('/list')">
+    {{ day }}
+</button>
         </div>
       </div>
-    </div>
+  
+      <div class="bg-white rounded-lg shadow p-4 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+        <header class="flex justify-between border-b-2 border-b-sky-900 p-2 items-center">
+          <button @click="decreaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-left-arrow-alt'></i>
+        </button>
+          <h2 class="text-xl font-bold">{{ currentMonth }} {{ currentYear }}</h2>
+          <button @click="increaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-right-arrow-alt'></i>
+        </button>
+        </header>
+        <h3 class="text-xl font-bold">Выполняю заказы в Зеленые дни</h3>
+        <div class="grid grid-cols-7  gap-2 mt-4 text-center font-bold">
+            
+          <div class="py-2">Пн</div>
+          <div class="py-2">Вт</div>
+          <div class="py-2">Ср</div>
+          <div class="py-2">Чт</div>
+          <div class="py-2">Пт</div>
+          <div class="py-2">Сб</div>
+          <div class="py-2">Вс</div>
+        </div>
+        <div class="grid grid-cols-7 gap-2 mt-4">
+            <template v-for="(value, index) in (firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1)" :key="index">
+    <div class="w-8 h-8"></div>
+</template>
+<button class="border-2 border-blue-800 rounded-full w-8 h-8 flex items-center justify-center" 
+        v-for="(day,) in daysInMonth" 
+        :key="day" 
+        :class="isOpen(day) ? ' bg-green-600 rounded-full w-8 h-8 flex items-center justify-center' : 'bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center'"
+        @click="isOpen(day) && $router.push('/list')">
+    {{ day }}
+</button>
+        </div>
+      </div>
+
+      <div class="bg-white rounded-lg shadow p-4 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+        <header class="flex justify-between border-b-2 border-b-sky-900 p-2 items-center">
+          <button @click="decreaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-left-arrow-alt'></i>
+        </button>
+          <h2 class="text-xl font-bold">{{ currentMonth }} {{ currentYear }}</h2>
+          <button @click="increaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-right-arrow-alt'></i>
+        </button>
+        </header>
+        <h3 class="text-xl font-bold">Выполняю заказы в Зеленые дни</h3>
+        <div class="grid grid-cols-7  gap-2 mt-4 text-center font-bold">
+            
+          <div class="py-2">Пн</div>
+          <div class="py-2">Вт</div>
+          <div class="py-2">Ср</div>
+          <div class="py-2">Чт</div>
+          <div class="py-2">Пт</div>
+          <div class="py-2">Сб</div>
+          <div class="py-2">Вс</div>
+        </div>
+        <div class="grid grid-cols-7 gap-2 mt-4">
+            <template v-for="(value, index) in (firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1)" :key="index">
+    <div class="w-8 h-8"></div>
+</template>
+<button class="border-2 border-blue-800 rounded-full w-8 h-8 flex items-center justify-center" 
+        v-for="(day,) in daysInMonth" 
+        :key="day" 
+        :class="isOpen(day) ? ' bg-green-600 rounded-full w-8 h-8 flex items-center justify-center' : 'bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center'"
+        @click="isOpen(day) && $router.push('/list')">
+    {{ day }}
+</button>
+        </div>
+      </div>
+      <div class="bg-white rounded-lg shadow p-4 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+        <header class="flex justify-between border-b-2 border-b-sky-900 p-2 items-center">
+          <button @click="decreaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-left-arrow-alt'></i>
+        </button>
+          <h2 class="text-xl font-bold">{{ currentMonth }} {{ currentYear }}</h2>
+          <button @click="increaseMonth" class="bg-sky-900 text-white rounded-full w-8 h-8 flex items-center justify-center">
+            <i class='bx bx-right-arrow-alt'></i>
+        </button>
+        </header>
+        <h3 class="text-xl font-bold">Выполняю заказы в Зеленые дни</h3>
+        <div class="grid grid-cols-7  gap-2 mt-4 text-center font-bold">
+            
+          <div class="py-2">Пн</div>
+          <div class="py-2">Вт</div>
+          <div class="py-2">Ср</div>
+          <div class="py-2">Чт</div>
+          <div class="py-2">Пт</div>
+          <div class="py-2">Сб</div>
+          <div class="py-2">Вс</div>
+        </div>
+        <div class="grid grid-cols-7 gap-2 mt-4">
+            <template v-for="(value, index) in (firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1)" :key="index">
+    <div class="w-8 h-8"></div>
+</template>
+<button class="border-2 border-blue-800 rounded-full w-8 h-8 flex items-center justify-center" 
+        v-for="(day,) in daysInMonth" 
+        :key="day" 
+        :class="isOpen(day) ? ' bg-green-600 rounded-full w-8 h-8 flex items-center justify-center' : 'bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center'"
+        @click="isOpen(day) && $router.push('/list')">
+    {{ day }}
+</button>
+        </div>
+      </div>
+  
+  
+</div> 
+        
+   
+    
 </template>
