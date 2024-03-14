da5a9026d179bac6d0e8384c4a68dc5d1975d8c1
Fodi999 <fodi85999@gmail.com>
2024-03-14 22:44:33 +0100

    header fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import store from '../store' // добавьте эту строку
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,13 +10,7 @@ const router = createRouter({
     {path: '/calendar',name: 'calendar',component: () => import('../components/CalendarVue.vue')},
     {path: '/drawer',name: 'drawer',component: () => import('../components/DrawerVue.vue')},
     {path: '/itcook',name: 'itcook',component: () => import('../components/ITcook.vue')},
-    {path: '/registration',name: 'registration',component: () => import('../components/RegistrationVue.vue')},
   ]
-})
-
-router.beforeEach((to, from, next) => {
-  if (to.name !== 'home' && !store.state.a.isAuthenticated) next({ name: 'home' }) // обратите внимание на изменение здесь
-  else next()
 })
 
 export default router
